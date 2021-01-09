acf70957a46b72f666025d06a46a569f99f57013
NiekRottier <niekrottier@gmail.com>
2021-01-09 16:38:47 +0100

    Able to create new notes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export class App extends React.Component {
         super()
         console.log("Hello World");
         this.state = {
-            user : "XXX"
+            user : "unknown"
         }
     }
 
@@ -25,7 +25,7 @@ export class App extends React.Component {
                 <h1>{this.state.user}'s notes</h1>
                 <div className="row-container">
                     <Namechanger changeName = {this.changeName} />
-                    <CreateNote />
+                    <CreateNote author={this.state.user} />
                 </div>
 
                 {/* <p>Wanna see your notes?</p> */}
--- src/CreateNote.js
+++ src/CreateNote.js
@@ -1,36 +1,78 @@
 import React from "react";
 import "./styles.css";
 import ReactModal from "react-modal";
+// import dateFormat from "dateformat"
 
 export class CreateNote extends React.Component {
     constructor(){
         super()
         console.log("Created CreateNote");
         this.state = {
-            modelOpen : false
+            modelOpen : false,
+            title : "",
+            body : "",
+            resData : {}
         }
 
         ReactModal.setAppElement('#root');
     }
 
+    // Open and Close Modal
     openModal = () => {
-        this.setState({
-            modelOpen : true
-        })
+        this.setState({ modelOpen : true })
         console.log("Opened Modal");
     }
 
     closeModal = () => {
-        this.setState({
-            modelOpen : false
-        })
+        this.setState({ modelOpen : false })
         console.log("Closed Modal");
+    }
+
+    // Handle Form Changes
+    handleTitleChange(event){
+        let formTitle = event.target.value
+        this.setState({ title : formTitle })
+    }
+    
+    handleBodyChange(event){
+        let formBody = event.target.value
+        this.setState({ body : formBody })
+    }
+
+    // POST request to API
+    handleSubmit = () => {
+        let requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+            },
+            body: JSON.stringify({
+                'title' : this.state.title,
+                'body' : this.state.body,
+                'author' : this.props.author
+            })
+        };
+
+        fetch("http://docent.cmi.hro.nl/bootb/demo/notes/", requestOptions)
+            .then(response => response.json())
+            .then(data => this.createdNote(data))
+            .catch(error => console.log(error));
+    }
+
+    createdNote(data){
+        console.log("Created a Note");
+        console.log(data);
+        this.setState({ resData : data })
+
+        window.location.reload()
+        this.closeModal()
     }
 
     render(){
         return (
             <div className="createNote">
-                <input type="button" value="Create Note" onClick={() => this.openModal()}/>
+                <input type="button" value="Create Note" onClick={this.openModal}/>
 
                 {/* Modal and all its settings */}
                 <ReactModal 
@@ -40,13 +82,17 @@ export class CreateNote extends React.Component {
                     onRequestClose={this.closeModal}>
                            
                     <form className="createNoteForm column-container">
-                        <label htmlFor="Title">Title</label>
-                        <input type="text" name="Title" />
+                        <label htmlFor="title">Title</label>
+                        <input type="text" name="title" minLength="3" value={this.state.title} 
+                            onChange={(event) => this.handleTitleChange(event)} />
 
-                        <label htmlFor="Body">Body</label>
-                        <input type="text" name="Body" />
+                        <label htmlFor="body">Body</label>
+                        <input type="text" name="body" minLength="3" value={this.state.body} 
+                            onChange={(event) => this.handleBodyChange(event)} />
 
-                        <input type="submit" name="Submit" value="Submit" readOnly />
+                        <p>Author = <i>{this.props.author}</i></p>
+
+                        <input type="button" value="Submit" onClick={this.handleSubmit}/>
                     </form>
 
                     <small><i>click esc to close</i></small> 
